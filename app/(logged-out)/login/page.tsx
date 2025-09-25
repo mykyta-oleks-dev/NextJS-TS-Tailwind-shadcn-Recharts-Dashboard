f72baa869d302a28ce9aac608be4fdb10ac76faf
Oleksiichuk Mykyta <mykyta.oleks.dev@gmail.com>
2025-09-25 12:59:14 +0300

    feat: set up form prototype

---
+++ app/(logged-out)/login/page.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card';
+import { PersonStandingIcon } from 'lucide-react';
+import Link from 'next/link';
+
+const LoginPage = () => {
+	return (
+		<>
+			<PersonStandingIcon size={50} />
+			<Card className="w-full max-w-sm">
+				<CardHeader>
+					<CardTitle>Login</CardTitle>
+					<CardDescription>
+						Log in to your SupportMe account
+					</CardDescription>
+				</CardHeader>
+				<CardContent>Login form</CardContent>
+				<CardFooter className="justify-between">
+					<small>Don&apos;t have an account?</small>
+					<Button asChild variant="outline" size="sm">
+						<Link href="/sign-up">Sign Up</Link>
+					</Button>
+				</CardFooter>
+			</Card>
+		</>
+	);
+};
+
+export default LoginPage;
