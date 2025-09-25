34f5fd5706d043bbeb19f85f036de4b1748a59d8
Oleksiichuk Mykyta <mykyta.oleks.dev@gmail.com>
2025-09-25 18:24:27 +0300

    feat: set up dashboard page structure

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,20 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+
 const DashboardPage = () => {
-	return <div>Dashboard</div>;
+	return (
+		<Tabs defaultValue="employees">
+			<TabsList>
+				<TabsTrigger value="employees">Employees stats</TabsTrigger>
+				<TabsTrigger value="teams">Teams stats</TabsTrigger>
+			</TabsList>
+			<TabsContent value="employees" className="mt-4">
+				<p>Employees stats will be displayed here.</p>
+			</TabsContent>
+			<TabsContent value="teams" className="mt-4">
+				<p>Teams stats will be displayed here.</p>
+			</TabsContent>
+		</Tabs>
+	);
 };
 
 export default DashboardPage;
