202b5b6db9824d6b1f171e7ad2ad272004dd183d
Oleksiichuk Mykyta <mykyta.oleks.dev@gmail.com>
2025-09-25 20:23:18 +0300

    feat: customized bar chart with tailwind classes

--- app/dashboard/components/employees-stats/chart.tsx
+++ app/dashboard/components/employees-stats/chart.tsx
@@ -23,7 +23,7 @@ const EmployeesStatsChart = () => {
 					<span>Employee work location trends</span>
 				</CardTitle>
 			</CardHeader>
-			<CardContent>
+			<CardContent className="px-0">
 				<ResponsiveContainer width="100%" height={350}>
 					<BarChart
 						data={data}
@@ -33,13 +33,47 @@ const EmployeesStatsChart = () => {
 							left: 20,
 							bottom: 5,
 						}}
+						className="[&_.recharts-tooltip-cursor]:fill-zinc-200 dark:[&_.recharts-tooltip-cursor]:fill-zinc-800"
 					>
 						<XAxis dataKey="name" stroke="#888" fontSize={12} />
-						<YAxis stroke='#888' fontSize={12} />
-						<Tooltip />
-						<Legend />
+						<YAxis stroke="#888" fontSize={12} />
+						<Tooltip
+							separator=": "
+							formatter={(value, name) => {
+								if (name === 'office')
+									return [value, 'Work from office'];
+								if (name === 'wfh')
+									return [value, 'Work from home'];
+								return [value, name];
+							}}
+							labelClassName='font-bold'
+							wrapperClassName="!text-sm dark:!bg-black rounded-md dark:!border-border"
+						/>
+						<Legend
+							iconType="circle"
+							formatter={(value) => {
+								if (value === 'office')
+									return (
+										<div className="text-sm">
+											Work from office
+										</div>
+									);
+								if (value === 'wfh')
+									return (
+										<div className="text-sm">
+											Work from home
+										</div>
+									);
+								return value;
+							}}
+						/>
 						<Bar dataKey="office" stackId="a" fill="#ec4899" />
-						<Bar dataKey="wfh" stackId="a" fill="#6b7280" radius={[4, 4, 0, 0]} />
+						<Bar
+							dataKey="wfh"
+							stackId="a"
+							fill="#6b7280"
+							radius={[4, 4, 0, 0]}
+						/>
 					</BarChart>
 				</ResponsiveContainer>
 			</CardContent>
